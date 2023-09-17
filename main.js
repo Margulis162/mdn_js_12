4da0500871be687865109e7f4141a5c55bcda992
Margulis162 <markmargulis@proton.me>
2023-09-17 19:53:42 -0400

    some coments

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 // creates a worker using a code from generate.js
 const hardworker = new Worker("./generate.js");
 
-document.querySelector('#generate').addEventListener('click', () => {
+document.querySelector('#generate').addEventListener('click', () => { 
+    /*querySelector is similar to getElementById in this case, but more versatile and can be used with different css selectors*/
+    // that gets number of primes to be generated from the input
     const quota = document.querySelector("#quota").value;
+    // sends generate command to the worker and the number of primes to be generated
     hardworker.postMessage({
         command:"generate",
         quota,
